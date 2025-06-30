6c2ea6fb875ff2712db2720f83253f174393111e
Александр Жуков <alexrabota44@mail.ru>
2025-06-30 13:58:41 +0300

    hw 18

--- src/index.js
+++ src/index.js
@@ -19,3 +19,18 @@ const swiper = new Swiper('.slider-teacher', {
   modules: [Navigation, Pagination],
   
 });
+
+let acc = document.querySelectorAll(".accordion__btn");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+};
